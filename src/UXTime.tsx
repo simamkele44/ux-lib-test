e86b47ba929f8c975626795fd67ef76bc5e0cd91
simanongwe <simamkele@ikhokha.com>
2022-12-02 09:56:32 +0200

    publish

--- src/UXTime.tsx
+++ src/UXTime.tsx
@@ -9,7 +9,7 @@ export const UXTime:React.FC<checkDateTime> = (time: checkDateTime) => {
   const asktime: checkDateTime = time;
   return (
     <div>
-      UX x Time:
+      UX Time:
       {TimeElapsed({time: asktime.time})}
     </div>
   );
