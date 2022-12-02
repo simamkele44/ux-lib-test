5e60dd56eed3a60bdf065890c111b2d21173484b
simanongwe <simamkele@ikhokha.com>
2022-12-02 10:00:25 +0200

    test: 💍 added month test
    sdnj
    
    BREAKING CHANGE: 🧨 no

--- test/month.test.tsx
+++ test/month.test.tsx
@@ -3,14 +3,14 @@ import TimeElapsed from '../src/TimeElapsed';
 describe('Past Month', () => {
   it('Test month in a different year but less than 12 months', () => {
     const time = new Date("2021-12-25T12:00:00");
-    expect(TimeElapsed({time: time})).toBe("11 months ago");
+    expect(TimeElapsed({time: time})).toBe("1 year ago");
   });
 });
 
 describe('This Month t', () => {
   it('Test the same month', () => {
     const time = new Date("2022-04-25T12:00:00");
-    expect(TimeElapsed({time: time})).toBe("Happening in 7 months");
+    expect(TimeElapsed({time: time})).toBe("Happening in 8 months");
   });
 });
 
