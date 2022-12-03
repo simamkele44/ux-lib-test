1a32a189f8c5d8803e71a5a0857c70a47164555f
simanongwe <simamkele@ikhokha.com>
2022-12-03 19:27:23 +0200

    feat: 🎸 klhjsdh
    sdgsldjg

--- src/UXTime.tsx
+++ src/UXTime.tsx
@@ -9,7 +9,7 @@ export const UXTime:React.FC<checkDateTime> = (time: checkDateTime) => {
   const asktime: checkDateTime = time;
   return (
     <div>
-      installed exece:
+      Time:
       {TimeElapsed({time: asktime.time})}
     </div>
   );
