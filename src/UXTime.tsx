c62e6816b4c271dc78f0053f6bd75a992f18a1ed
simanongwe <simamkele@ikhokha.com>
2022-12-06 21:46:50 +0200

    fix: 🐛 fixed bug that made things non
    I just fixed a buf
    
    BREAKING CHANGE: 🧨 no

--- src/UXTime.tsx
+++ src/UXTime.tsx
@@ -9,7 +9,7 @@ export const UXTime:React.FC<checkDateTime> = (time: checkDateTime) => {
   const asktime: checkDateTime = time;
   return (
     <div>
-      User Time:
+      UserX Time:
       {TimeElapsed({time: asktime.time})}
     </div>
   );
