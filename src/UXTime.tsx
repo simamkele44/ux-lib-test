e9accc8951e4e5cd7858506d96af5f94a5f89d4f
simanongwe <simamkele@ikhokha.com>
2022-12-06 23:25:32 +0200

    feat: 🎸 released code
    sdhjksd
    
    BREAKING CHANGE: 🧨 no

--- src/UXTime.tsx
+++ src/UXTime.tsx
@@ -9,7 +9,7 @@ export const UXTime:React.FC<checkDateTime> = (time: checkDateTime) => {
   const asktime: checkDateTime = time;
   return (
     <div>
-      User U bTime:
+      User bTime:
       {TimeElapsed({time: asktime.time})}
     </div>
   );
