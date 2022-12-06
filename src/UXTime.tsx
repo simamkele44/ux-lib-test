6396d756c671ec5644fd779d51f12afd9877aee7
simanongwe <simamkele@ikhokha.com>
2022-12-06 22:38:55 +0200

    fix: 🐛 gfxgh
    ghfxv

--- src/UXTime.tsx
+++ src/UXTime.tsx
@@ -9,7 +9,7 @@ export const UXTime:React.FC<checkDateTime> = (time: checkDateTime) => {
   const asktime: checkDateTime = time;
   return (
     <div>
-      User U Time:
+      User U b Time:
       {TimeElapsed({time: asktime.time})}
     </div>
   );
